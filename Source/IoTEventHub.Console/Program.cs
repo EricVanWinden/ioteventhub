2dbe3ff47a4ae08d0e81f346953b18d4ca5db885
Eric van Winden <Eric.VanWinden@akzonobel.com>
2021-01-17 19:54:55 +0100

    Added console application that can run on my pi

---
+++ Source/IoTEventHub.Console/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
+using System;
+
+namespace IoTEventHub.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Substribe to logging
+            Common.StatisticsSingleton.Instance.StatusLogged += SendToApplicationInsights;
+            Common.StatisticsSingleton.Instance.LogMessage("Subscribed to logger");
+            var config = Common.StatisticsSingleton.Instance.HubConfig;
+
+            // Create processor
+            var storageContainerName = "messagehost";
+            var consumerGroupName = PartitionReceiver.DefaultConsumerGroupName;
+            var _processor = new EventProcessorHost(config.HubName, consumerGroupName, config.IotHubConnectionString, config.StorageConnectionString, storageContainerName);
+            _processor.RegisterEventProcessorAsync<Common.LoggingEventProcessor>();
+
+            System.Console.ReadLine();
+
+            // Save data and unregister the event processor
+            Common.StatisticsSingleton.Instance.Save();
+            _processor.UnregisterEventProcessorAsync();
+        }
+
+        /// <summary>
+        /// The logger
+        /// </summary>
+        /// <param name="message"></param>
+        public static void SendToApplicationInsights(string message)
+        {
+            System.Console.WriteLine($"{DateTime.Now}: {message}");
+        }
+    }
+}
